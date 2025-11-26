3b2fd5b8d8afb190074d9391f9ce18bd8ab36762
Faerryn <alexandre.liao@gmail.com>
2025-11-26 03:16:18 -0500

    add some utility functions for performing operations on entire models

---
+++ src/fedmat/fedutils.py
@@ -0,0 +1,25 @@
+from copy import deepcopy
+
+import torch
+from torch import Tensor
+from torch import nn
+from typing import List
+from collections.abc import Callable
+
+ReduceFn = Callable[[List[Tensor]], Tensor]
+
+def reduce(
+        inputs: List[nn.Module],
+        reduce_fn: ReduceFn,
+        output: Optional[nn.Module] = None,
+        ) -> nn.Module:
+    if output is None:
+        if len(inputs) == 0:
+            raise ValueError("reduce needs at least one input model to determine output model shape")
+        output = deepcopy(inputs[0])
+    for name, parameter in output.named_parameters():
+        parameter.copy_(reduce_fn([input.get_parameter(name) for input in inputs]))
+    return output
+
+def torch2reduce(fn: Callable[[Tensor], Tensor]) -> ReduceFn:
+    return lambda tensors: fn(torch.stack(tensors), axis=0)
