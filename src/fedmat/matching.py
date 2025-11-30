ddb6f7687ea2e0e130d2d434a1ad3f96788a918a
Faerryn <alexandre.liao@gmail.com>
2025-11-29 21:46:58 -0500

    Add a matching base class and some stubs to work on

---
+++ src/fedmat/matching.py
@@ -0,0 +1,24 @@
+from abc import ABC
+from torch import nn
+from typing import List, Tuple
+
+class Matcher(ABC):
+    def match(client_blocks: List[nn.Module]) -> List[Tuple[int, int]]:
+        """Match the client blocks to each other to minimize some distance metric."""
+        # TODO: gooder documentation
+        pass
+
+class GreedyMatcher(ABC):
+    def match(client_blocks: List[nn.Module]) -> List[Tuple[int, int]]:
+        """Le Greedy"""
+        pass
+
+class PseudoHungarianMatcher(ABC):
+    def match(client_blocks: List[nn.Module]) -> List[Tuple[int, int]]:
+        """Le Hungarian ala FedMA"""
+        pass
+
+class PerfectMatcher(ABC):
+    def match(client_blocks: List[nn.Module]) -> List[Tuple[int, int]]:
+        """Use some quantum error correction package to do matching"""
+        pass
